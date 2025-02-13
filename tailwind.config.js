bd0e9b9e98e4d6a53a38ef03811da822ed1a6fa7
Konrad Klein <konra@KKWork.KK.local>
2025-02-13 10:02:56 +0100

    projekt Struktur

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{vue,ts}',
+    './components/**/*.{vue,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
