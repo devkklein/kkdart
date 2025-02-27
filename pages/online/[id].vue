dc2bfe3fc5dddabd44182d949e0cc09691ce72bb
Konrad Klein <konrad@14bea858-5d8c-4945-bb75-e29773f132f4.fritz.box>
2025-02-27 22:23:22 +0100

    m

--- pages/online/[id].vue
+++ pages/online/[id].vue
@@ -1,57 +1,67 @@
 <template>
- <div class="flex h-screen  ">
-  <Sidebar />
-  <div class=" h-full w-full ">
-    <PopupsLeaveMatch :showPopup="showLeave" @close="showLeave = false" />
-    <PopupsBullOffWinner v-if="viewBullOffWinner" :winner="bullOffWinner" />
-    <PopupsWinnerPopup v-if="matchWinner" :winner="matchWinner" />
-    <div v-if="matchStarted && match" class="w-full p-4 ">
-      <div class="w-full bg-secondary-300 border-2 border-primary flex space-x-10 text-sm justify-center rounded-xl py-1 shadow-xl px-3">
-                <p>In Mode: {{ match.settings.inMode }}</p>
-                <p>Out Mode: {{ match.settings.outMode }}</p>
-                <p>Bull Off: {{ match.settings.bullOff }}</p>
-                <div><p >Legs: {{ match.settings.legCount }}</p></div>
-                <div><p>Sets: {{ match.settings.setCount }}</p></div>
-                <div><p>Max Rounds: {{ match.settings.maxRounds }}</p></div>
-                <div><p>BaseScore: {{ match.settings.baseScore }}</p></div>
-      </div>
-      <div class="flex flex-col justify-end">
-        <div class="flex ">
-          <div class="flex w-8/12 flex-col">
-            <div class="grid grid-flow-col py-4 space-x-5">
-              <div v-for="(player, playerIndex) in match.players" :key="playerIndex"
-              >
-                
-                 
+  <div class="flex h-screen  ">
+    <Sidebar />
+    <div class=" h-full w-full ">
+      <PopupsLeaveMatch :showPopup="showLeave" @close="showLeave = false" />
+      <PopupsBullOffWinner v-if="viewBullOffWinner" :winner="bullOffWinner" />
+      <PopupsWinnerPopup v-if="matchWinner" :winner="matchWinner" />
+      <div v-if="matchStarted && match" class="w-full p-4 ">
+        <div
+          class="w-full bg-secondary-300 border-2 border-primary flex space-x-10 text-sm justify-center rounded-xl py-1 shadow-xl px-3">
+          <p>In Mode: {{ match.settings.inMode }}</p>
+          <p>Out Mode: {{ match.settings.outMode }}</p>
+          <p>Bull Off: {{ match.settings.bullOff }}</p>
+          <div>
+            <p>Legs: {{ match.settings.legCount }}</p>
+          </div>
+          <div>
+            <p>Sets: {{ match.settings.setCount }}</p>
+          </div>
+          <div>
+            <p>Max Rounds: {{ match.settings.maxRounds }}</p>
+          </div>
+          <div>
+            <p>BaseScore: {{ match.settings.baseScore }}</p>
+          </div>
+        </div>
+        <div class="flex flex-col justify-end">
+          <div class="flex ">
+            <div class="flex w-8/12 flex-col">
+              <div class="grid grid-flow-col py-4 space-x-5">
+                <div v-for="(player, playerIndex) in match.players" :key="playerIndex">
 
-                  <div class=" flex w-full flex-col items-center bg-secondary-300  justify-center bg-secondary border-2 p-4 rounded-xl shadow-xl "
-                  :class="match.currentPlayerIndex === playerIndex ? 'border-blue-500' : 'border-primary'">
+
+
+                  <div
+                    class=" flex w-full flex-col items-center bg-secondary-300  justify-center bg-secondary border-2 p-4 rounded-xl shadow-xl "
+                    :class="match.currentPlayerIndex === playerIndex ? 'border-blue-500' : 'border-primary'">
                     <div class="flex">
-                    <div class="flex flex-col items-center justify-center">
-                      <GameProfile :User="player" />
-                      <div class="flex space-x-3">
-                        <p>Legs: {{ player.scores.legsWon }}</p>
-                        <p>Sets: {{ player.scores.setsWon }}</p>
+                      <div class="flex flex-col items-center justify-center">
+                        <GameProfile :User="player" />
+                        <div class="flex space-x-3">
+                          <p>Legs: {{ player.scores.legsWon }}</p>
+                          <p>Sets: {{ player.scores.setsWon }}</p>
+                        </div>
+                        <h1 class="text-8xl">{{ player.scores.currentScore }}</h1>
+
                       </div>
-                      <h1 class="text-8xl">{{ player.scores.currentScore }}</h1>
-                       
-                    </div>
-                    <div
-                    class=" w-16 h-auto max-h-80 overflow-hidden relative flex flex-col items-center justify-end ">
-                    <div class="absolute top-0 left-0 w-full h-16 bg-gradient-to-b from-secondary-300 to-transparent"></div>
-                    <div v-for="(score, index) in player.scores.legScores" :key="index">
-                      <h1 class="text-gray-300 text-xs text-nowrap ">Leg: {{ index }}</h1>
-                      <div v-for="(s, i) in score.roundScores" :key="i" class="text-xl">
-                        <p>{{s.scores.reduce((acc, val) => acc + val, 0)}}</p>
+                      <div
+                        class=" w-16 h-auto max-h-80 overflow-hidden relative flex flex-col items-center justify-end ">
+                        <div
+                          class="absolute top-0 left-0 w-full h-16 bg-gradient-to-b from-secondary-300 to-transparent">
+                        </div>
+                        <div v-for="(score, index) in player.scores.legScores" :key="index">
+                          <h1 class="text-gray-300 text-xs text-nowrap ">Leg: {{ index }}</h1>
+                          <div v-for="(s, i) in score.roundScores" :key="i" class="text-xl">
+                            <p>{{s.scores.reduce((acc, val) => acc + val, 0)}}</p>
+                          </div>
+                        </div>
+
                       </div>
                     </div>
-                   
-                  </div>
-                </div>
 
                     <div class="flex w-full justify-center space-x-2 mt-4">
                       <div v-for="dartNum in 3" :key="dartNum"
-                       
                         class="w-24 h-20 p-1 border-2 border-primary rounded flex items-center justify-center">
                         <div v-if="player.scores.dartScores[dartNum].multiplier"
                           class="flex flex-col justify-center items-center">
@@ -71,67 +81,68 @@
                       </div>
                     </div>
                   </div>
-                
-              </div>
-            </div>
 
-           <GameInputButtons v-if="bullOffFinished" @score="score" />
-            <div v-if="!bullOffFinished">
-              <div class="flex flex-col items-center justify-center space-y-4">
-                <h1>Normal Bull Off</h1>
-                <div class="flex space-x-4">
-                  <button @click="bullOffScoring(0, 1, 0)" class=" p-4 rounded-xl shadow-xl">00</button>
-                  <button @click="bullOffScoring(25, 1, 25)" class=" p-4 rounded-xl bg-green-500 shadow-xl">25</button>
-                  <button @click="bullOffScoring(25, 2, 50)" class="p-4 rounded-xl bg-red-500 shadow-xl">50</button>
                 </div>
-                <h1 v-if="bullOffTie" class="text-2xl"> BullOff TIE! Starting next round... </h1>
               </div>
 
+              <GameInputButtons v-if="bullOffFinished" @score="score" />
+              <div v-if="!bullOffFinished">
+                <div class="flex flex-col items-center justify-center space-y-4">
+                  <h1>Normal Bull Off</h1>
+                  <div class="flex space-x-4">
+                    <button @click="bullOffScoring(0, 1, 0)" class=" p-4 rounded-xl shadow-xl">00</button>
+                    <button @click="bullOffScoring(25, 1, 25)"
+                      class=" p-4 rounded-xl bg-green-500 shadow-xl">25</button>
+                    <button @click="bullOffScoring(25, 2, 50)" class="p-4 rounded-xl bg-red-500 shadow-xl">50</button>
+                  </div>
+                  <h1 v-if="bullOffTie" class="text-2xl"> BullOff TIE! Starting next round... </h1>
+                </div>
+
+              </div>
+              <div></div>
             </div>
-            <div></div>
-          </div>
-          <div class=" w-8/12 flex py-4 pl-4  pr-0  rounded-xl ">
-            <div  class="flex flex-col  w-full ">
-              
-
-              <GameScoreChart :player="match.players[getPlayerIndex(match)]" />
-              <GameLiveStatsChart :player="match.players[getPlayerIndex(match)]" />
+            <div class=" w-8/12 flex py-4 pl-4  pr-0  rounded-xl ">
+              <div class="flex flex-col  w-full ">
 
 
-            </div>
-            <div>
+                <GameScoreChart :player="match.players[getPlayerIndex(match)]" />
+                <GameLiveStatsChart :player="match.players[getPlayerIndex(match)]" />
+
+
+              </div>
+              <div>
+
+              </div>
 
             </div>
 
           </div>
 
         </div>
-
       </div>
-    </div>
-    <div v-else class="flex flex-col h-full w-full justify-center items-center">
-      <h1>Waiting for Enemy player</h1>
-      <GameMatchList />
-    </div>
+      <div v-else class="flex flex-col h-full w-full justify-center items-center">
+        <h1>Waiting for Enemy player</h1>
+        <GameMatchList />
+      </div>
 
+    </div>
+    <footer class="absolute bottom-0 p-4 w-full flex justify-around">
+      <div class="bg-primary flex w-1/8 justify-start items-center rounded-xl shadow-xl px-3">
+        <button @click="leaveMatch" class="flex items-center justify-center p-3 rounded ">
+          <Icon name="mdi:logout" size="20" class="scale-x-[-1] text-red-500" />
+          <span class="ml-2">Leave Match</span>
+        </button>
+      </div>
+      <div class="bg-primary flex w-1/8 justify-end items-center rounded-xl shadow-xl px-3 space-x-4">
+        <button class="flex items-center justify-center p-3 rounded ">
+          <Icon name="mdi:cog" size="20" class="text-white" />
+        </button>
+        <button class="flex items-center justify-center p-3 rounded ">
+          <Icon name="mdi:chat" size="20" class="text-white" />
+        </button>
+      </div>
+    </footer>
   </div>
-  <footer class="absolute bottom-0 p-4 w-full flex justify-around">
-    <div class="bg-primary flex w-1/8 justify-start items-center rounded-xl shadow-xl px-3">
-      <button @click="leaveMatch" class="flex items-center justify-center p-3 rounded ">
-        <Icon name="mdi:logout" size="20" class="scale-x-[-1] text-red-500" />
-        <span class="ml-2">Leave Match</span>
-      </button>
-    </div>
-    <div class="bg-primary flex w-1/8 justify-end items-center rounded-xl shadow-xl px-3 space-x-4">
-      <button class="flex items-center justify-center p-3 rounded ">
-        <Icon name="mdi:cog" size="20" class="text-white" />
-      </button>
-      <button class="flex items-center justify-center p-3 rounded ">
-        <Icon name="mdi:chat" size="20" class="text-white" />
-      </button>
-    </div>
-  </footer>
-</div>
 </template>
 
 <script lang="ts" setup>
@@ -293,88 +304,88 @@ const match = ref<Match | null>(null);
 //const match = ref<Match | null>(null);
 // websocket functions
 const joinMatch = (matchIdVal: string, role?: 'Spieler1' | 'Spieler2') => {
-  
-  
-  
-    ws.value = new WebSocket(
-      `${window.location.protocol === 'https:' ? 'wss://' : 'ws://'}${window.location.host}/api/webSocket/ws`
-    );
-  
-    ws.value.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      if (data.type === "match-joined") {
-        if (!playerRole.value) {
-          playerRole.value = role ? role : 'Spieler2';
+
+
+
+  ws.value = new WebSocket(
+    `${window.location.protocol === 'https:' ? 'wss://' : 'ws://'}${window.location.host}/api/webSocket/ws`
+  );
+
+  ws.value.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    if (data.type === "match-joined") {
+      if (!playerRole.value) {
+        playerRole.value = role ? role : 'Spieler2';
+      }
+    }
+    if (data.type === "match-start") {
+      matchStarted.value = true;
+      matchId.value = data.matchId;
+      match.value = data.match;
+      currentPlayerIndex.value = data.match.currentPlayerIndex;
+    }
+    if (data.type === "bulloff-update") {
+
+      match.value = data.match;
+
+    }
+    if (data.type === "switch-turn") {
+
+      match.value = data.match;
+
+    }
+    if (data.type === "bulloff-tie") {
+      bullOffTie.value = true;
+      setTimeout(() => {
+        bullOffTie.value = false;
+        currentPlayerIndex.value = 0;
+        match.value = data.match;
+      }, 3000);
+
+    }
+    if (data.type === "bulloff-winner") {
+      bullOffFinished.value = true;
+      bullOffWinner.value = data.winner;
+      match.value = data.match;
+
+      match.value?.currentPlayerIndex === 2;
+      setTimeout(() => {
+        viewBullOffWinner.value = true;
+      }, 1000);
+
+      setTimeout(() => {
+        viewBullOffWinner.value = false;
+        if (!data.match.currentPlayerIndex) {
+          match.value?.currentPlayerIndex === 0;
         }
-      }
-      if (data.type === "match-start") {
-        matchStarted.value = true;
-        matchId.value = data.matchId;
+        else {
+          match.value?.currentPlayerIndex === 1;
+        }
         match.value = data.match;
-        currentPlayerIndex.value = data.match.currentPlayerIndex;
-      }
-      if (data.type === "bulloff-update") {
-  
-        match.value = data.match;
-  
-      }
-      if (data.type === "switch-turn") {
-        
-        match.value = data.match;
-        
-      }
-      if (data.type === "bulloff-tie") {
-        bullOffTie.value = true;
-        setTimeout(() => {
-          bullOffTie.value = false;
-          currentPlayerIndex.value = 0;
-          match.value = data.match;
-        }, 3000);
-  
-      }
-      if (data.type === "bulloff-winner") {
-        bullOffFinished.value = true;
-        bullOffWinner.value = data.winner;
-        match.value = data.match;
-  
-        match.value?.currentPlayerIndex === 2;
-        setTimeout(() => {
-          viewBullOffWinner.value = true;
-        }, 1000);
-  
-        setTimeout(() => {
-          viewBullOffWinner.value = false;
-          if (!data.match.currentPlayerIndex) {
-            match.value?.currentPlayerIndex === 0;
-          }
-          else {
-            match.value?.currentPlayerIndex === 1;
-          }
-          match.value = data.match;
-        }, 3000);
-      }
-      if (data.type === "dart-update") {
-        match.value = data.match;
-        
-      }
-      
-  
-      if (data.type === "leg-update") {
-       match.value = data.match;
-        
-      }
-      if (data.type === "match-finished") {
-        match.value = data.match;
-        match.value?.currentPlayerIndex === 2;
-        matchWinner.value = data.winner;
-      }
-  
-  
-    };
-  
-    ws.value.onopen = () => {
-      ws.value?.send(JSON.stringify({ type: 'join-match', matchId: matchIdVal, player: { id: user.id, username: user.username, image: user.profileImage || null } }));
-    };
+      }, 3000);
+    }
+    if (data.type === "dart-update") {
+      match.value = data.match;
+
+    }
+
+
+    if (data.type === "leg-update") {
+      match.value = data.match;
+
+    }
+    if (data.type === "match-finished") {
+      match.value = data.match;
+      match.value?.currentPlayerIndex === 2;
+      matchWinner.value = data.winner;
+    }
+
+
+  };
+
+  ws.value.onopen = () => {
+    ws.value?.send(JSON.stringify({ type: 'join-match', matchId: matchIdVal, player: { id: user.id, username: user.username, image: user.profileImage || null } }));
+  };
 };
 
 
@@ -386,6 +397,7 @@ onMounted(() => {
     if (route.query.creator === 'true') {
       joinMatch(matchId.value, 'Spieler1');
     } else {
+      //
       joinMatch(matchId.value);
     }
   }
@@ -397,9 +409,9 @@ onMounted(() => {
   });
 });
 // Function to get player index for the current user
-function getPlayerIndex(match:Match): number {
+function getPlayerIndex(match: Match): number {
   if (!match || !user.id) return -1;
-  
+
   const index = match.players.findIndex((player) => player.id === user.id);
   return index;
 }
@@ -432,8 +444,7 @@ function score(score: number, multiplier: number, points: number) {
 </script>
 
 <style scoped>
-.grid-container{
+.grid-container {
   direction: rtl;
 }
-
 </style>
